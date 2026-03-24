af6973e5e546e843159b00d506ddd653afa51c87
XUEXUE717 <13658291738@163.com>
2026-03-24 17:18:49 +0800

    Initial commit

--- day12-函数版学生管理系统/main.py
+++ day12-函数版学生管理系统/main.py
@@ -13,6 +13,7 @@ def add(now_user):
             mess = {user:password}
             Amessage = Amessage | mess
             print(Amessage)
+            menu(now_user)
 
         case '2': #添加学生信息
             while True:
@@ -104,11 +105,68 @@ def up_data(now_user):
                         menu(now_user)
                     case 'D':
                         menu(now_user)
+#显示查询数据
+def find_data(now_user):
+    match now_user:
+        case '1':
+            while True:
+                user = input("请输入需要查询密码的账号")
+                if user in Amessage:
+                    print(f"密码：{Amessage[user]}")
+                    menu(now_user)
+                else:
+                    print("账号不存在，重新输入\n")
+        case '2':
+            while True:
+                f_data = input("请选择查询方式:A学号 B姓名 C性别 D班级 E返回到菜单").strip()
+                match f_data:
+                    case 'A':
+                        user = int(input("请输入需要查询的学号").strip())
+                        estimate = -1
+                        for x in students:
+                            if x[0] == user:
+                                print(x)
+                                estimate = 0
+                        if estimate == -1:
+                            print("学号不存在，请重新输入")
+                    case 'B':
+                        user = input("请输入需要查询的姓名").strip()
+                        estimate = -1
+                        for x in students:
+                            if x[1] == user:
+                                print(x)
+                                estimate = 0
+                        if estimate == -1:
+                            print("姓名不存在，请重新输入")
+                    case 'C':
+                        user = input("请输入需要查询的性别F M").strip()
+                        estimate = -1
+                        for x in students:
+                            if x[2] == user:
+                                print(x)
+                                estimate = 0
+                        if estimate == -1:
+                            print("输入错误，请重新输入")
+                    case 'D':
+                        user = input("请输入需要查询的班级信息").strip()
+                        estimate = -1
+                        for x in students:
+                            if x[3] == user:
+                                print(x)
+                                estimate = 0
+                        if estimate == -1:
+                            print("班级不存在，请重新输入")
+                    case 'E':
+                        menu(now_user)
+
+#退出登陆
+def quit_data():
+    sign()
 
 
 #菜单栏
 def menu(now_user):
-    input_num = input("请选择功能：\nA.增加数据\nB.删除数据\nC，更新数据\nD.删除数据\nE.显示全部数据\nF.退出登陆")
+    input_num = input("请选择功能：\nA.增加数据\nB.删除数据\nC，更新数据\nD.删除数据\nE.查找数据\nF.退出登陆")
     match input_num:
         case 'A':
             add(now_user)
@@ -116,26 +174,35 @@ def menu(now_user):
             dele(now_user)
         case 'C':
             up_data(now_user)
+        case 'D':
+            dele(now_user)
+        case 'E':
+            find_data(now_user)
+        case 'F':
+            quit_data()
 
-while True :
-    num = input("1，管理员  2.教师\n请选择账号登录：\n")
-    if num != '1' and num != '2':
-        raise ValueError("输入错误！")
-    elif num == '1':
-        user1 = input("请输入管理员账号")
-        password1 = input("请输入管理员密码")
-        is_all_in = all(key in Amessage and Amessage[key] == value for key,value in {user1:password1}.items())   #判断一个小字典的所有键值对，是否都存在与另一个大字典里
-        if is_all_in:
-            print("登陆成功！请选择功能")
-            menu(num)
+def sign():#登陆
+    while True :
+        num = input("1，管理员  2.教师\n请选择账号登录：\n")
+        if num != '1' and num != '2':
+            raise ValueError("输入错误！")
+        elif num == '1':
+            user1 = input("请输入管理员账号")
+            password1 = input("请输入管理员密码")
+            is_all_in = all(key in Amessage and Amessage[key] == value for key,value in {user1:password1}.items())   #判断一个小字典的所有键值对，是否都存在与另一个大字典里
+            if is_all_in:
+                print("登陆成功！请选择功能")
+                menu(num)
+            else:
+                raise ValueError("账号或者密码错误")
         else:
-            raise ValueError("账号或者密码错误")
-    else:
-        user1 = input("请输入教师账号")
-        password1 = input("请输入教师密码")
-        is_all_in = all(key in Amessage and Amessage[key] == value for key, value in {user1: password1}.items())  # 判断一个小字典的所有键值对，是否都存在与另一个大字典里
-        if is_all_in:
-            print("登陆成功！请选择功能")
-            menu(num)
-        else:
-            raise ValueError("账号或者密码错误")
+            user1 = input("请输入教师账号")
+            password1 = input("请输入教师密码")
+            is_all_in = all(key in Amessage and Amessage[key] == value for key, value in {user1: password1}.items())  # 判断一个小字典的所有键值对，是否都存在与另一个大字典里
+            if is_all_in:
+                print("登陆成功！请选择功能")
+                menu(num)
+            else:
+                raise ValueError("账号或者密码错误")
+
+sign()

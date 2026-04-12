a2bf7dfcfce24df6f80bf327d8deaf071fdcb290
XUEXUE717 <13658291738@163.com>
2026-04-12 14:25:55 +0800

    Initial commit

--- day13-面向对象/类的方法.py
+++ day13-面向对象/类的方法.py
@@ -23,3 +23,37 @@ class people:
 #实例化类
 p = people('runoob',10,30)
 p.speak()
+
+#__init__：初始化方法（构造函数），用来在对象创建时设置初始状态
+class Student:
+    #初始化方法：在创建对象时自动调用，用来定义属性
+    def __init__(self,name,student_id):
+        self.name = name#实例属性：名字
+        self.student_id = student_id#实例属性：学号
+        self.scores = [] #实例属性：成绩列表，默认为空
+#自定义方法study
+#功能模拟学习行为，打印信息
+def study(self,subject):
+    print(f"{self.name}正在努力学习{subject}...")
+
+#自定义方法：do_homework(self,homework_name),模拟完成作业打印信息
+def do_homework(self,homework_name):
+    print(f"{self.name}正在完成{homework_name}")
+
+
+#自定义方法：check_score
+#功能：检查成绩，根据分数返回评价
+def check_score(self,score):
+    self.scores.append(score)#将分数增加到列表中
+    if score >= 90:
+        print(f"当前共有{len(self.scores)}次成就记录")
+        return  '优秀'
+    elif score >= 60:
+        print(f"当前共有{len(self.scores)}次成就记录")
+        return '及格'
+    else:
+        print(f"当前共有{len(self.scores)}次成就记录")
+        return "不及格"
+#1.实例化对象（创建具体的学生），这里自动触发__init__方法
+student_a = Student("张三",'1001')
+student_b = Student("李四",'1002')
